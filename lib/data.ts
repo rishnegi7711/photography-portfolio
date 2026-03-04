398acc3ba2e78acdb9d70891b76ca6a7716b69ef
rishnegi7711 <rishnegi28@gmail.com>
2026-03-04 09:50:12 +0000

    feat: project setup with Next.js, Tailwind, shadcn, and Hygraph integration

---
+++ lib/data.ts
@@ -0,0 +1,28 @@
+const fetchData = async () => {
+  const data = await fetch(`${process.env.HYGRAPH_ENDPOINT}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query: `query myQuery{
+          photos{
+            title
+            id
+            camera
+            lens
+            featured
+         		picture{
+              url
+            }
+          }
+        }`,
+    }),
+  });
+
+  const parsedData = await data.json();
+  console.log(JSON.stringify(parsedData, null, 2));
+};
+
+export default fetchData;

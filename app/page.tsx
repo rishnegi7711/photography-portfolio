f8c579e844c13b60dafd424b74693059e56deb3b
rishnegi7711 <rishnegi28@gmail.com>
2026-05-06 13:26:33 +0100

    feat: add hover effect to masonry layout gallery images

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default async function Home() {
       <div className="columns-3 sm:columns-1 md:columns-2 lg:columns-4">
         {photos.map((photo) => {
           return (
-            <div key={photo.id}>
+            <div key={photo.id} className="overflow-hidden">
               <Image
                 src={photo.picture.url}
                 alt={photo.title}
-                className="w-full h-auto object-cover mb-7 "
+                className="object-cover mb-7 hover:scale-105 transition-transform duration-300 w-full h-full"
                 height={photo.picture.height}
                 width={photo.picture.width}
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
